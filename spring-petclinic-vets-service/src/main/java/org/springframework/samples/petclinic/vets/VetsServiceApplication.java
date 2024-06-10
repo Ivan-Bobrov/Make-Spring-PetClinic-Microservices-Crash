1ddada686c7af5435d9ad79388e819ed3169384d
Eugen <107585180+Eugen163@users.noreply.github.com>
2024-06-10 10:16:35 +0200

    feat(vets): fixed slow response by caching output

--- spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
+++ spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/VetsServiceApplication.java
@@ -18,6 +18,7 @@ package org.springframework.samples.petclinic.vets;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.samples.petclinic.vets.system.VetsProperties;
 
@@ -27,6 +28,7 @@ import org.springframework.samples.petclinic.vets.system.VetsProperties;
 @EnableDiscoveryClient
 @SpringBootApplication
 @EnableConfigurationProperties(VetsProperties.class)
+@EnableCaching
 public class VetsServiceApplication {
 
 	public static void main(String[] args) {
--- spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/system/CacheConfig.java
+++ spring-petclinic-vets-service/src/main/java/org/springframework/samples/petclinic/vets/system/CacheConfig.java
@@ -15,16 +15,31 @@
  */
 package org.springframework.samples.petclinic.vets.system;
 
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * Cache could be disable in unit test.
  * @author Maciej Szarlinski
  */
 @Configuration
-@EnableCaching
 @Profile("production")
 class CacheConfig {
+
+    @Bean
+    public CacheManager cacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(Caffeine.newBuilder()
+            .expireAfterWrite(30, TimeUnit.MINUTES)
+            .maximumSize(1000));
+        return cacheManager;
+    }
 }
+
