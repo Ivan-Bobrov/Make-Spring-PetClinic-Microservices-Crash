919dd7932e341f971cc76db0da7c7dcf19ee642a
Matthias Marschalk <matthias.marschalk@haw-hamburg.de>
2024-06-24 11:27:52 +0200

    feat(blocked threads): add visible loading state

--- spring-petclinic-api-gateway/src/main/resources/static/scripts/pet-record/pet-record.controller.js
+++ spring-petclinic-api-gateway/src/main/resources/static/scripts/pet-record/pet-record.controller.js
@@ -1,11 +1,14 @@
 'use strict';
 
 angular.module('petRecord')
-    .controller('PetRecordController', ['$http', '$state', '$stateParams', function ($http, $state , $stateParams) {
+    .controller('PetRecordController', ['$http', '$state', '$stateParams', '$scope', function ($http, $state , $stateParams, $scope) {
         const self = this;
 
+        $scope.loading = true;
         $http.get(`api/customer/pet-records/${$stateParams.petId}?vetId=${$stateParams.vetId}`).then(function (resp) {
             self.record = resp.data;
+        }).finally(function () {
+            $scope.loading = false;
         });
 
         if(!$stateParams.petId || !$stateParams.vetId) {
