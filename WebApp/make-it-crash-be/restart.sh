dfbfe131ac0ecdbf5c145086b9593f5a5588033c
Leon Sauer <leonsauer0@gmail.com>
2024-07-01 10:24:32 +0200

    Start Chaos monkey with starting Script

--- WebApp/make-it-crash-be/restart.sh
+++ WebApp/make-it-crash-be/restart.sh
@@ -27,6 +27,9 @@ restart_service() {
     echo ${password} | sudo -S docker-compose -f ${file} down
     echo ${password} | sudo -S docker-compose -f ${file} pull
     echo ${password} | sudo -S docker-compose -f ${file} up -d
+    if [${SERVER_IP2} == ${server_ip}]; then
+          echo ${password} | sudo -S ./scripts/chaos/call_chaos.sh vets attacks_enable_latency watcher_enable_restcontroller
+        fi
 EOF
 }
 
